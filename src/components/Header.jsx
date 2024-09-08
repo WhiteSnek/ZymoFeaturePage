0b1de0c52c1b369abf195c6472f064b91ba42c0c
WhiteSnake <whitesnake35711@gmail.com>
2024-09-08 18:47:12 +0530

    added responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
     <header className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <Link to='/'>
-          <img src='https://zymo.app/static/media/zymoNewLogo.4e597c5ad4734f840906.png' alt='logo' className='h-8' />
+          <img src='https://zymo.app/static/media/zymoNewLogo.4e597c5ad4734f840906.png' alt='logo' className='h-3 sm:h-8' />
         </Link>
         <nav>
-          <ul className="nav-list text-lg">
+          <ul className="nav-list text-xs sm:text-lg">
             <li><Link to="/fleet">Fleet</Link></li>
             <li><Link to="/about">About Us</Link></li>
             <li><Link to="/blogs">Blogs</Link></li>
